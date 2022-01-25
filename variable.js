9a8b0d4c8c36831be0d5147867a6350769cff614
uthe99240 <uthe99240@gmail.com>
2022-01-26 01:46:25 +0600

    commit

---
+++ variable.js
@@ -0,0 +1,11 @@
+var price = 21;
+var age = 24;
+var temperature = 26;
+
+var name = "faisal ahmed";
+var address = "vooter goli kocu khet"
+var gf = 'alia bhatt';
+
+var pass = true;
+var subscribed = false;
+
